8bd6ea67199c556514ccf9b59e43c5d56d555824
Dhairya Shah <dhairyaj2004@gmail.com>
2023-09-07 20:14:08 +0530

    commit

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+
+// import logo from "./src/components/logo-jsc(1).png"
+const Navbar = () => {
+  
+    return ( 
+        <div className="navbar">
+               {/* <img src={logo} alt="logo" /> */}
+            <h1>JUNIOR SCIENCE COLLEGE</h1>
+         <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/Material">Study Material</Link>
+            <Link to="/Enroll">Buy a Course</Link>
+            <Link to="/success">Success Stories</Link>
+         </div>
+
+        </div>
+     );
+}
+ 
+export default Navbar;
